bd5737ac71d6185064776abfa0f0dc144f6f3d2c
rossbates <rbates@gmail.com>
2015-04-02 14:18:41 -0500

    pep cleanup

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ example.py
 This module contains an example of how to instantiate and use the EchoAPI
 """
 
+
 from echoes import EchoAPI
 import json
 
@@ -13,23 +14,23 @@ with open('config.json') as json_data_file:
 	config = json.load(json_data_file)
 
 try:
-	echo = EchoAPI(config['email'],config['password'])
+	echo = EchoAPI(config['email'], config['password'])
 except Exception, e:
 	print e.message
 	exit()
 
 if echo.logged_in:
-	#cards are searches, Q&A, etc...
+	# cards are searches, Q&A, etc...
 	cards = echo.cards(50)
 	for c in cards:
 		print c
 
-	#todos have a type of shopping or task
-	todos = echo.todos('SHOPPING_ITEM',10)
+	# todos have a type of shopping or task
+	todos = echo.todos('SHOPPING_ITEM', 10)
 	for t in todos:
 		print t
 
-	tasks = echo.todos('TASK',10)
+	tasks = echo.todos('TASK', 10)
 	for t in tasks:
 		print t
 
